148199b33e1850bdce31ea6cf995892aa67d0aa2
Pete Warnock <petewarnock@gmail.com>
2025-12-22 18:25:18 -0800

    test: Simple root page without redirect

--- apps/frontend/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from 'next/navigation'
-
-// Force dynamic rendering to avoid caching
-export const dynamic = 'force-dynamic'
-
-// Simple redirect to home page
-export default function RootPage() {
-  redirect('/home')
-}
